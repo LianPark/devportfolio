dba40e822b751b7731b68d344bbb1965ce550e51
Lian Park <woori01@gmail.com>
2024-05-27 09:45:36 +0900

    Gulp 파일 수정 package.json의 버전 업데이트

--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,28 @@
-var gulp = require('gulp');
-var plumber = require('gulp-plumber');
-const uglify = require('gulp-uglify');
-const sass = require('gulp-sass');
-const wait = require('gulp-wait');
-const babel = require('gulp-babel');;
-const rename = require('gulp-rename');
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const sourcemaps = require('gulp-sourcemaps');
 
-gulp.task('scripts', function() {
-    return gulp.src('./js/scripts.js')
-        .pipe(plumber(plumber({
-            errorHandler: function (err) {
-                console.log(err);
-                this.emit('end');
-            }
-        })))
-        .pipe(babel({
-          presets: [['@babel/env', {modules:false}]]
-        }))
-        .pipe(uglify({
-            output: {
-                comments: '/^!/'
-            }
-        }))
-        .pipe(rename({extname: '.min.js'}))
-        .pipe(gulp.dest('./js'));
+gulp.task('sass', () => {
+    return gulp
+      .src('./scss/styles.scss')        // scss 파일 경로 설정
+      .pipe(sourcemaps.init())
+      .pipe(sass().on('error', sass.logError))
+      .pipe(sourcemaps.write(''))       // 컴파일 된 css map 파일 저장될 경로 설정
+      .pipe(gulp.dest('./css'));        // 컴파일 된 css 파일 저장될 경로 설정
 });
 
-gulp.task('styles', function () {
-    return gulp.src('./scss/styles.scss')
-        .pipe(wait(250))
-        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe(gulp.dest('./css'));
+// 컴파일 상시 감시
+gulp.task('watch', () => {
+    gulp.watch('./scss/styles.scss', gulp.series('sass'));
 });
 
-gulp.task('watch', function() {
-    gulp.watch('./js/scripts.js', gulp.series('scripts'));
-    gulp.watch('./scss/styles.scss', gulp.series('styles'));
+
+gulp.task('sass2', () => {
+  return gulp
+    .src('./scss/styles.scss')
+    .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
 });
+
+//exports.buildStyles = buildStyles;
+gulp.task('default', gulp.series('sass', 'watch'));
